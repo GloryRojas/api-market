48d7f65e960e122e474b2ec1516733f256087421
Aldair <aldairsosa18@gmail.com>
2019-10-23 21:37:04 -0500

    adding user and owner routes

--- main/models/products.js
+++ main/models/products.js
@@ -1,10 +1,31 @@
 let product = [
     {
-        idProduct: 2,
+        idProduct: 1,
+        tipeUser: '1',
         name : 'cargador',
         price : '200',
-        descriptcion
+        descriptcion : 'quick charge'
     },
-    {}
+
+    {
+        idProduct: 2,
+        tipeUser: 2,
+        name : 'celular',
+        price : '500',
+        descriptcion : 'Xiaomi'
+    },
+
+    {
+        idProduct: 3,
+        tipeUser: 2,
+        name : 'laptop',
+        price : '2500',
+        descriptcion : 'asus'
+    },
+
 
 ]
+
+module.exports = product;
+
+
--- main/routes/routes.js
+++ main/routes/routes.js
@@ -1,12 +1,26 @@
 const router = require('express').Router();
+const product = require('../models/products')
+
 
 router.get('/', (req, res) =>{
     res.send('hello world')
+    console.log(product);
 })
 
-router.get('/user', (req, res) =>{
+router.get('/addProduct/user/:idUser', (req, res) =>{ 
 
-    
+    console.log(product);
+    const idU = req.params.idUser;
+    res.send(`this is ur user id = ${idU}`);
+
+})
+
+router.get('/addProduct/owner/:idOwner', (req, res) =>{
+
+    console.log(product[2]);
+    const idOwner = req.params.idOwner;
+    res.send(idOwner);
+
 })
 
 module.exports = router
